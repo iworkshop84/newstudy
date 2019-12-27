cba99664ef54c13a9d1c64aebe4376a07ea69088
iworkshop <iworkshop84@gmail.com>
2019-12-28 00:20:33 +0200

    добавление новости, изменение структуры БД

--- add.php
+++ add.php
@@ -1,2 +1,46 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+include_once __DIR__ . '/core/sql.php';
+
+
+if(!empty($_POST)){
+
+    if(!empty($_POST['title'])){
+    $postnews = News_add($_POST);
+        if($postnews){
+            header('Location: /index.php');
+        }
+    }
+}
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Добавить новость</title>
+</head>
+<body>
+<h1>Добавить новость</h1>
+
+<form action="/add.php" method="post" enctype="application/x-www-form-urlencoded">
+    <p>Название статьи:</p>
+        <input type="text" name="title">
+
+    <p>Текст статьи:</p>
+        <textarea rows="10" cols="70" name="text"></textarea>
+
+
+    <p><input type="submit" name="send" value="Опубликовать"></p>
+
+</form>
+</body>
+</html>
